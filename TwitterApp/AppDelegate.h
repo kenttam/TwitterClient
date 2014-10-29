233852022aa06c45b003b83d3a0a80b507461e42
Kent Tam <ktam39@gmail.com>
2014-10-28 21:47:23 -0700

    Initial Commit

---
+++ TwitterApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwitterApp
+//
+//  Created by Kent Tam on 10/28/14.
+//  Copyright (c) 2014 Kent Tam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TwitterApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TwitterApp
+//
+//  Created by Kent Tam on 10/28/14.
+//  Copyright (c) 2014 Kent Tam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
